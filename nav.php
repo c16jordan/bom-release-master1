360da361d8350cbd266d7fcfa6f16a03ec203d2e
Mathalomew <jc8768is@metrostate.edu>
2019-10-10 01:03:15 -0500

    Sbom tree table
    After changing the Jquery version in the nav.php file I still cannot get the buttons to work. The code functions outside the Basic Composer website in a separate file. Apart from that needing to get those to work, is this what the assignment FP3 is?

--- nav.php
+++ nav.php
@@ -11,14 +11,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="styles/main_style.css" type="text/css">
         <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <!-- jQuery library Updated 10/08/2019-->
-        <script src="jquery-3.4.1.js"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
         <!-- Latest compiled JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="styles/custom_nav.css" type="text/css">
         <title>A Basic Composer</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet"/>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet"/>
         <link rel="stylesheet" href="./mainStyleSheet.css">
     </head>
--- scanner_sbom_tree.php
+++ scanner_sbom_tree.php
@@ -7,10 +7,20 @@
   
  ?>
  
+ <script>
+ 
+/*
+if (typeof jQuery !== 'undefined') {  
+    // jQuery is loaded => print the version
+    alert(jQuery.fn.jquery);
+}
+*/
+
+ </script>
  	<link rel="stylesheet" href="css/screen.css" media="screen" />
 	<link rel="stylesheet" href="css/jquery.treetable.css" />
 	<link rel="stylesheet" href="css/jquery.treetable.theme.default.css" />
-	<script src="jquery-3.4.1.js"></script>
+
 		
 		
 		<div class="right-content">
@@ -20,66 +30,145 @@
 				
 				
 				
-			<div id="trees">
-
-				<button id="expand">Exp</button>
-		
-				<script>
-			
-					$("#expand").click( function(){
-						$("#test").treetable("expandAll");
-						//alert("expand");
-					});
-			
-				</script>
-		
-				<table id="test">
-
-				<caption>Practice Tree</caption>
-				<thead>
-				<tr>
-					<th>Tree column</th>
-					<th>Additional data</th>
-					<th>more info</th>
-				</tr>
-				</thead>
-				<tbody>
-				<tr data-tt-id="1">
-					<td>Node 1: Click on the icon in front of me to expand this branch.</td>
-					<td>I live in the second column.</td>
-					<td>I live in the third column.</td>
-				</tr>
-				<tr data-tt-id="1.1" data-tt-parent-id="1">
-					<td>Node 1.1: Look, I am a table row <em>and</em> I am part of a tree!</td>
-					<td>Interesting.</td>
-				<td>I live in the third column.</td>
-				</tr>
-				<tr data-tt-id="1.1.1" data-tt-parent-id="1.1">
-					<td>Node 1.1.1: I am part of the tree too!</td>
-					<td>That's it!</td>
-					<td>I live in the third column.</td>
-				</tr>
-				<tr data-tt-id="2">
-					<td>Node 2: I am another root node, but without children</td>
-					<td>Hurray!</td>
-					<td>I live in the third column.</td>
-				</tr>
-				</tbody>
-	  
-			</table>
-		
-		<script src="jquery.treetable.js"></script>
-			
-		<script>
-			$("#test").treetable({ expandable: true });
-		</script>		
-					
+	<div id="trees">
 	
+	<button id="expando" style="font-size: 10px">Expand</button>
+	<button id="reducto" style="font-size: 10px">Collapse</button>
+	
+	<script>
+		$(document).ready(function(){
+				$("#expando").click(function(){
+					$("#sbom_tree").treetable('expandAll');
+				});
+		});
+		
+		$(document).ready(function(){
+				$("#reducto").click(function(){
+					$("#sbom_tree").treetable('collapseAll');
+				});
+		});
+	</script>
+	
+		<!-- Collect database information to load tables-->
+	<?php 
+	
+	
+	// Collect database column names
+	
+	$sql = "SELECT DISTINCT COLUMN_NAME 
+			FROM INFORMATION_SCHEMA.COLUMNS
+			WHERE TABLE_NAME = 'sbom'";
+	
+	$result_col = $db->query($sql);
+	$columns = [];
+	
+	if ($result_col->num_rows > 0) {
+		while($row = $result_col->fetch_assoc()) {
+			$columns[] = $row['COLUMN_NAME'];
+		}
+	}
+	
+	
+	// Collect database node information
+	
+	$sql = "SELECT * from sbom ORDER BY 'app_name';";
+	$result = $db->query($sql);
+	$bom_ary;	// To be an associative array
+	$iterator = []; // Inner numerically indexed array for autoindexing
+	$key;
+                if ($result->num_rows > 0) {
+                   
+                    while($row = $result->fetch_assoc()) {
+						
+						$key = $row["app_name"]." ".$row["app_version"];
+						$value = $row["cmp_id"].",".$row["cmp_name"].",".$row["cmp_version"].",".$row["cmp_type"].","
+								 .$row["app_status"].",".$row["cmp_status"].",".$row["notes"];
+								 
+						$bom_ary[$key][] = explode(",", $value);
+                    }
+                }
+                else {
+                    echo "0 results";
+                }
+     $result->close();
+     ?>
+
+ 
+ <!-- Fill table rows -->
+ <div>
+ 
+ <table id="sbom_tree">
+	<thead>
+	<tr>
+	<?php 
+		echo "<th>Software</th>";
+		// Set up the columns by name
+		foreach($columns as $key=>$value){
+			if($key < 4){
+				continue;
+			}
+				echo "<th>".$value."</th>";
+		}
+	?>
+	</tr>
+	</thead>
+	
+	<tbody>
+	
+	<?php	
+		//Place the data in the table tree per parent-child relationships 
+		$parent = 1;
+
+		foreach($bom_ary as $key=>$value){
+			
+			// Parent node - Software(Name + Version)
+			echo '<tr data-tt-id="'.$parent.'">
+					<td>'.$key.'</td> 
+				  </tr>';
+			
+			
+			//Array of child nodes
+			$rows = $value;
+			
+			// Access array storing children node data arrays
+			for($child_index = 0, $out_ary_length = count($rows); $child_index < $out_ary_length;$child_index++){
+				
+				echo '<tr data-tt-id="'.$parent.'.'.($child_index+1).'" data-tt-parent-id="'.$parent.'">';
+				echo '<td></td>';
+				$child_node = $rows[$child_index];
+				
+				//Access child node data
+				for($child = 0, $in_ary_length = count($rows[0]); $child < $in_ary_length; $child++){
+					
+					echo '<td>'.$child_node[$child].'</td>';
+
+				}
+				
+				echo '</tr>';	
+			}
+			$parent++;
+		}
+	
+		?>
+		</tbody>
+	
+		</table>	
+
+		<script src="jquery.treetable.js"></script>
+		
+		<script>
+			$("#sbom_tree").treetable({ expandable: true });
+		</script>	
 			
 		</div>
 				
+
 	
-				
+		
+ </div>
+			
+
+			
 	</div>
 </div>
 
