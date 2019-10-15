1597b1520c74203688c409074c4b094bd2f94f01
Mathalomew <jc8768is@metrostate.edu>
2019-10-15 01:29:04 -0500

    Update left_menu_reports.php
    Minor left menu name change

--- left_menu_reports.php
+++ left_menu_reports.php
@@ -4,7 +4,7 @@
 		<div <?php if($left_selected == "REPORTS")
 		{ echo 'class="menu-left-current-page"'; } ?>>
 		<img src="./images/work_in_progress.jpg">
-		<br/>Pie Chart<br/></div>
+		<br/>BOM Reports<br/></div>
 	</a>
 	
 	<a href="reports_bar_chart.php">
