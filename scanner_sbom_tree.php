8edae5bbd5332f48338ca9ae869593e19557e3c6
Mathalomew <jc8768is@metrostate.edu>
2019-10-22 16:59:13 -0500

    Rework FP8 (not done yet)
    Dumping php, css, and javascript to bottom of page and modularized so it can be dumped into external files
    
    Prep work for redoing FP8

--- scanner_sbom_tree.php
+++ scanner_sbom_tree.php
@@ -4,285 +4,70 @@
   $left_selected = "SBOMTREE";
 
   include("./nav.php");   
+  global $db;
  ?>
 
  
-    <link rel="stylesheet" href="css/screen.css" media="screen" />
-    <link rel="stylesheet" href="css/jquery.treetable.css" />
-    <link rel="stylesheet" href="css/jquery.treetable.theme.default.css" />
-	<script src="jquery-3.4.1.js"></script>
+ <link rel="stylesheet" href="css/screen.css" media="screen" />
+ <link rel="stylesheet" href="css/jquery.treetable.css" />
+ <link rel="stylesheet" href="css/jquery.treetable.theme.default.css" />
+ <link rel="stylesheet" type="text/css" href="mycss.css">
+ <script src="jquery-3.4.1.js"></script>
+ 
+ <script>
+	var flag = 0;
+	var rootOrigColor;
+	var childOrigColor;
+	var leafOrigColor;
+ </script>	
  
 
-		
-		<div class="right-content">
-			<div class="container">
+<div class="right-content">
+	<div class="container">
 	
-				<h3 style = "color: #01B0F1;">Scanner --> BOM Tree</h3>
-				
-	<script>
-		var flag = 0;
-		var rootOrigColor;
-		var childOrigColor;
-		var leafOrigColor;
-	</script>	
-
-	<?php 
-	
-	
-	$sql = "SELECT app_name, app_version, app_status, 
-				   cmp_name, cmp_version, cmp_type, cmp_status,
-				   request_id, request_date, request_status, request_step,
-				   notes 
-			FROM sbom";
-			
-	$result = $db->query($sql);
-	
-	
-	$bom_ary;	// Not so nice 3-dimensional array that stores BOM table data. But, because no searching is involved you just enter the associative keys to access the data.
-	$base_key;	// Stores base node data
-	$root_key;	// Stores root node data
-    $autocomplete = []; // Stores all app and cmp names 
-	$autocomplete_num = []; // Numerically indexed keys from $autocomplete
- 		
-		if ($result->num_rows > 0) {
-                   
-			while($row = $result->fetch_assoc()) {
-			
-			// Store data for autocomplete
-			$autocomplete[$row["app_name"]] = 1;
-			$autocomplete[$row["cmp_name"]] = 1;
-			
-			// Store relevant components by Application (name+id)
-			
-			$base_key = $row["app_name"];
-			$root_key = $row["app_name"]." ".$row["app_version"]."@".$row["app_status"];
-			$child_key = $row["cmp_name"]." ".$row["cmp_version"];
-			
-			//$row["app_status"]
-			$value = $row["cmp_type"]."@".$row["cmp_status"]
-				."@".$row["request_id"]."@".$row["request_date"]."@".$row["request_status"]."@".$row["request_step"]
-				."@".$row["notes"];
-								 
-				$bom_ary[$base_key][$root_key][$child_key][] = explode("@", $value);
-            }
-         }
-         else {
-            echo "0 results";
-         }
-		 
-	 // Convert $autocomplete associative array into a numerically index array for easier access
-	
-	 foreach($autocomplete as $name=>$value){
-		 $autocomplete_num[] = $name;
-	 }
-	 sort($autocomplete_num);
-	 
-     $result->close();
-     ?>
+		<h3 style = "color: #01B0F1;">Scanner --> BOM Tree</h3>
+ 
 <!-- https://www.w3schools.com/howto/howto_js_autocomplete.asp -->
-<style>
-
-#where{
-	margin-right: 5px;
-}
-
-#error{
-	color: red;
-	font-size: 10px;
-	display: inline-block;
-	left:0;
-	position: relative;
-}
-
-.autocomplete{
-	width:150px; 
-	display:inline-block;
-	position:absolute;
-}
-
-.autocomplete-list{
-	width:200px;
-	position: absolute;
-	top:100%;
-	left:0;
-	right:0;
-	z-index:99;
-	background-color: #fff;
-}
-
-.autocomplete-items{
-	border-top: none;
-	border-left: 2px solid #f9f9f9;
-	border-bottom: none;
-	background-color: #f9f9f9;	
-}
-
-.root{
-	background-color: #f9f9f9;
-}
-
-.root_colored{
-	background-color: #e60000;
-}
-
-
-.child{
-	background-color: #f9f9f9;
-}
-
-
-.child_colored{
-	background-color: #ffff4d;
-}
-
-.leaf{
-	background-color: white;
-}
-
-.leaf_colored{
-	background-color: #009900;
-}
-
-.highlight_node{
-	background-color: #1E90FF;
-}
-
-</style>
  
- <!-- Fill table rows -->
-
  <table id="sbom_tree">
  
-	
 	<div>
 	
-	<caption>
-		<button id="expand" style="font-size: 10px">Expand All</button>
-		<button id="collapse" style="font-size: 10px">Collapse All</button>
-		<button id="colorize" style="font-size: 10px"> Toggle Color </button>
-		<button id="reds" style="font-size: 10px">Reds</button>
-		<button id="red_yellow" style="font-size: 10px"> Reds and Yellows </button>
-		<button id="where_button" style="font-size: 10px; margin-left:25px">Where used</button>
+		<caption>
+			<button id="expand" style="font-size: 10px">Expand All</button>
+			<button id="collapse" style="font-size: 10px">Collapse All</button>
+			<button id="colorize" style="font-size: 10px"> Toggle Color </button>
+			<button id="reds" style="font-size: 10px">Reds</button>
+			<button id="red_yellow" style="font-size: 10px"> Reds and Yellows </button>
+			<button id="where_button" style="font-size: 10px; margin-left:25px">Where used</button>
 	
-	<!--
-		Probably abandon this autocomplete drop down list
-		<div class="autocomplete">
-			
-		<div id="autocomplete-list" class="autocomplete-items"><input></input></div> 
-		</div>
-	-->
+			<!--
+				Probably abandon this autocomplete drop down list
+				<div class="autocomplete">
+				
+				<div id="autocomplete-list" class="autocomplete-items"><input></input></div> 
+				</div>
+			-->
 		
-		<input id="where_used" type="text" placeholder="name;[version id] option"></input>
-		<span id="error"></span>
+			<input id="where_used" type="text" placeholder="name;[version id] option"></input>
+			<span id="error"></span>
 		
-	</caption>
+		</caption>
 	
 	
 	
 	</div>
 	
-	
-	
-	
 	<thead>
+	
 	<tr>
-	<?php 
-		// Set up the columns by name
-		
-		echo "<th>Application</th>";
-		echo "<th>Application Status</th>";
-		echo "<th>Component Type</th>";
-		echo "<th>Component Status</th>";
-		echo "<th>Request Id</th>";
-		echo "<th>Request Date</th>";
-		echo "<th>Request Status</th>";
-		echo "<th>Request Step</th>";
-		echo "<th>Notes</th>";
-	?>
+		<?php setupTheaders(); ?>
 	</tr>
+	
 	</thead>
 	
 	<tbody>
-	<?php
-		// Set up the root nodes
-
-		$parent_id=1;
-		$root_id = 1;
-		$child_id = 1;
-		
-		ksort($bom_ary);
-		
-		// Set up base - App names only
-		foreach($bom_ary as $base=>$root_ary){
-			
-			base($base, $parent_id);
-			
-			// Set up root - App names + Versions only
-			foreach($root_ary as $root=>$cmp_array){
-				root($root, $parent_id, $root_id);
-				
-				$child_parent = $parent_id.'.'.$root_id;
-				
-				// Set up component - Cmp Name + Versions only
-				foreach($cmp_array as $child=>$cmp_values){
-				
-					child($child, $cmp_values, $child_parent ,$child_id);	
-					$child_id++;
-				}
-				
-				$child_id = 1;
-				$root_id++;
-			}
-			$root_id = 1;
-			$parent_id++;
-		}
-		
-		//<tr data-tt-id="x">
-		function base($base, $parent_id){
-			echo '<tr class="root '.$base.'" data-tt-id="'.$parent_id.'">';
-			echo '<td class="root">'.$base.'</td>';
-			
-			for($index=0; $index < 8; $index++){
-					echo '<td class="root"></td>';
-			}
-			
-			echo "</tr>";
-		}
-
-		//<tr data-tt-id="x.x">
-		function root($root, $parent_id, $root_id){
-		    $root = explode("@",$root);
-			echo '<tr class="child '.$root[0].'" data-tt-id="'.$parent_id.'.'.$root_id.'" data-tt-parent-id="'.$parent_id.'">';
-				
-				echo '<td class="child">'.$root[0].'</td>';
-				echo '<td class="child">'.$root[1].'</td>';
-				
-				for($index=0; $index < 7; $index++){
-					echo '<td class="child"></td>';
-				}
-			echo '</tr>';
-		}
-
-		//<tr data-tt-id="x.x.x">
-		function child($child, $child_ary, $parent_id, $child_id){
-			echo '<tr class="leaf '.$child.'" data-tt-id="'.$parent_id.'.'.$child_id.'" data-tt-parent-id="'.$parent_id.'">';
-				echo '<td class="leaf">'.$child.'</td>';	
-				
-				foreach($child_ary as $leaf=>$data)
-					leaf($data);
-					
-			echo '</tr>';
-		}
-
-		// Prints out leaf node data under children of child() function
-		function leaf($leaf_ary){
-			echo '<td class="leaf"></td>';
-			
-			foreach($leaf_ary as $key=>$value){
-				echo '<td class="leaf">'.$value.'</td>';
-			}
-	    }
-?>	
+		<?php phpMakeTree($db); ?>	
 	</tbody>
 	
 </table>	
@@ -607,6 +392,177 @@
 			
 	</div>
 </div>
+
+
+<?php 
+	
+	// Gather data for tree in four dimensions [base info][root info][child info][leaf info]
+	function callDB($db){
+	
+		$sql = "SELECT app_name, app_version, app_status, 
+					cmp_name, cmp_version, cmp_type, cmp_status,
+					request_id, request_date, request_status, request_step,
+					notes 
+				FROM sbom";
+			
+		$result = $db->query($sql);
+	
+	
+		$bom_ary;	// Not so nice 3-dimensional array that stores BOM table data. But, because no searching is involved you just enter the associative keys to access the data.
+		$base_key;	// Stores base node data
+		$root_key;	// Stores root node data
+		$autocomplete = []; // Stores all app and cmp names 
+		$autocomplete_num = []; // Numerically indexed keys from $autocomplete
+ 		
+		if ($result->num_rows > 0) {
+                   
+			while($row = $result->fetch_assoc()) {
+			
+			// Store data for autocomplete
+			$autocomplete[$row["app_name"]] = 1;
+			$autocomplete[$row["cmp_name"]] = 1;
+			
+			// Store relevant components by Application (name+id)
+			
+			$base_key = $row["app_name"];
+			$root_key = $row["app_name"]." ".$row["app_version"]."@".$row["app_status"];
+			$child_key = $row["cmp_name"]." ".$row["cmp_version"];
+			
+			//$row["app_status"]
+			$value = $row["cmp_type"]."@".$row["cmp_status"]
+				."@".$row["request_id"]."@".$row["request_date"]."@".$row["request_status"]."@".$row["request_step"]
+				."@".$row["notes"];
+								 
+				$bom_ary[$base_key][$root_key][$child_key][] = explode("@", $value);
+            }
+         }
+         else {
+            echo "0 results";
+         }
+		 
+		// Convert $autocomplete associative array into a numerically index array for easier access
+		
+		foreach($autocomplete as $name=>$value){
+		 $autocomplete_num[] = $name;
+		}
+	 
+		sort($autocomplete_num);
+		$result->close();
+	 
+		return $bom_ary;
+	}
+		
+	function setupTHeaders(){
+			
+			// Set up the columns by name
+		
+			echo "<th>Application</th>";
+			echo "<th>Application Status</th>";
+			echo "<th>Component Type</th>";
+			echo "<th>Component Status</th>";
+			echo "<th>Request Id</th>";
+			echo "<th>Request Date</th>";
+			echo "<th>Request Status</th>";
+			echo "<th>Request Step</th>";
+			echo "<th>Notes</th>";
+	}
+
+	
+	// Set up the root nodes
+		function phpMakeTree($db){
+			
+			$parent_id=1;
+			$root_id = 1;
+			$child_id = 1;
+		
+			$bom_ary = callDB($db);
+		
+			ksort($bom_ary);
+		
+			// Set up base - App names only
+			foreach($bom_ary as $base=>$root_ary){
+			
+				base($base, $parent_id);
+			
+				// Set up root - App names + Versions only
+				foreach($root_ary as $root=>$cmp_array){
+					root($root, $parent_id, $root_id);
+				
+					$child_parent = $parent_id.'.'.$root_id;
+				
+					// Set up component - Cmp Name + Versions only
+					foreach($cmp_array as $child=>$cmp_values){
+				
+						child($child, $cmp_values, $child_parent ,$child_id);	
+						$child_id++;
+					}
+				
+					$child_id = 1;
+					$root_id++;
+				}
+			
+				$root_id = 1;
+				$parent_id++;
+			}
+		
+		}
+	
+	
+		//<tr data-tt-id="x">
+	function base($base, $parent_id){
+			
+		echo '<tr class="root '.$base.'" data-tt-id="'.$parent_id.'">';
+		echo '<td class="root">'.$base.'</td>';
+		
+		for($index=0; $index < 8; $index++){
+				echo '<td class="root"></td>';
+		}
+		
+		echo "</tr>";
+		
+	}
+
+	//<tr data-tt-id="x.x">
+	function root($root, $parent_id, $root_id){
+		
+	    $root = explode("@",$root);
+		echo '<tr class="child '.$root[0].'" data-tt-id="'.$parent_id.'.'.$root_id.'" data-tt-parent-id="'.$parent_id.'">';
+				
+			echo '<td class="child">'.$root[0].'</td>';
+			echo '<td class="child">'.$root[1].'</td>';
+				
+			for($index=0; $index < 7; $index++){
+				echo '<td class="child"></td>';
+			}
+		echo '</tr>';
+		
+	}
+
+	//<tr data-tt-id="x.x.x">
+	function child($child, $child_ary, $parent_id, $child_id){
+		echo '<tr class="leaf '.$child.'" data-tt-id="'.$parent_id.'.'.$child_id.'" data-tt-parent-id="'.$parent_id.'">';
+			echo '<td class="leaf">'.$child.'</td>';	
+			
+			foreach($child_ary as $leaf=>$data)
+				leaf($data);
+				
+		echo '</tr>';
+	}
+		// Prints out leaf node data under children of child() function
+	function leaf($leaf_ary){
+		echo '<td class="leaf"></td>';
+		
+		foreach($leaf_ary as $key=>$value){
+			echo '<td class="leaf">'.$value.'</td>';
+		}
+    }
+	
+	
+	
+?>
+
+
+
 
 <p>
 <?php
