cfc1aa2f500b32d57530a71c371025ea60c2b5f6
Mathalomew <jc8768is@metrostate.edu>
2019-11-03 11:52:07 -0600

    Some cleanup

--- left_menu_reports.php
+++ left_menu_reports.php
@@ -3,14 +3,16 @@
 	<a href="reports_pi_chart.php">
 		<div <?php if($left_selected == "REPORTS")
 		{ echo 'class="menu-left-current-page"'; } ?>>
-		<img src="./images/work_in_progress.jpg">
+		<!-- http://www.clker.com/cliparts/4/e/5/a/1206557176743993576mcol_pie_chart.svg.hi.png pichart -->
+		<img src="./images/piechart.png">
 		<br/>BOM Reports<br/></div>
 	</a>
 	
 	<a href="reports_bar_chart.php">
 		<div <?php if($left_selected == "REPORTS")
 		{ echo 'class="menu-left-current-page"'; } ?>>
-		<img src="./images/work_in_progress.jpg">
+	   <!-- http://www.clker.com/cliparts/J/L/3/D/K/k/bar-chart-hi.png -->
+		<img src="./images/barchart.png">
 		<br/>Bar Chart<br/></div>
 	</a>
   
